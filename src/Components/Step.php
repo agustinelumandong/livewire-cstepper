bb2bfc325682cc25ca5d89f612f967f5a311a843
Cshan <agustinelumandong@gmail.com>
2025-09-26 20:02:44 +0800

    Add Step.php for backward compatibility with class alias to StepComponent

---
+++ src/Components/Step.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace agustinelumandong\LivewireCstepper\Components;
+
+// This file is kept for backward compatibility
+// Please use StepComponent instead
+
+class_alias(StepComponent::class, Step::class);
