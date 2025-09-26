a61e45b701185b4d8671c46ae94f5e6d45959280
Cshan <agustinelumandong@gmail.com>
2025-09-26 20:03:34 +0800

    Add HandlesSteps class alias for backward compatibility with HandlesNavigation

---
+++ src/Traits/HandlesSteps.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace agustinelumandong\LivewireCstepper\Traits;
+
+// This file is kept for backward compatibility
+// Please use HandlesNavigation instead
+
+class_alias(HandlesNavigation::class, HandlesSteps::class);
