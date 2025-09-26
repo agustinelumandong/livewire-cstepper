55073f905ee5dd44278e49fecb542026e207ff8e
Cshan <agustinelumandong@gmail.com>
2025-09-26 20:03:00 +0800

    Add StepContract for backward compatibility with class alias to StepperContract

---
+++ src/Contracts/StepContract.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace agustinelumandong\LivewireCstepper\Contracts;
+
+// This file is kept for backward compatibility
+// Please use StepperContract instead
+
+class_alias(StepperContract::class, StepContract::class);
